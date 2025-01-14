82ebc58b0ce4956c51500dd601052067a0c0e918
kanav789 <kanavrana91@gmail.com>
2025-01-14 10:56:02 +0530

    creating the function for generating the audio

---
+++ generatingmedia.js
@@ -0,0 +1,16 @@
+const youtubedl = require("youtube-dl-exec");
+const downloadMedia = async (url) => {
+  try {
+    console.log("Downloading audio...");
+    await youtubedl(url, {
+      output: "./public/audio.mp3",
+      format: "bestaudio",
+      audioFormat: "mp3",
+    });
+
+    console.log("Audio download complete ✅");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = downloadMedia;
